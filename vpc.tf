6b9259feb10535fba343641b46f9eed698a66c7a
mossey <nandwa.moses@gmail.com>
2020-08-02 19:44:25 +0300

    Create 3 vpcs and 3 subnets in each vpc

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,33 @@
 #
 # VPC Resources
-#  * VPC
+#  * VPCs
 #  * Subnets
 #  * Internet Gateway
-#  * Route Table
+#  * Route Table in the compute subnet
 #
 
 resource "aws_vpc" "compute_vpc" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.1.0.0/16"
 
   tags = map(
-    "Name", "terraform-eks-demo-node",
+    "Name", "compute-vpc",
     "kubernetes.io/cluster/${var.cluster-name}", "shared",
+  )
+}
+
+resource "aws_vpc" "database_vpc" {
+  cidr_block = "10.2.0.0/16"
+
+  tags = map(
+    "Name", "databse-vpc"
+  )
+}
+
+resource "aws_vpc" "connectivity_vpc" {
+  cidr_block = "10.3.0.0/16"
+
+  tags = map(
+    "Name", "connectivity-vpc"
   )
 }
 
@@ -19,20 +35,42 @@ resource "aws_subnet" "compute_subnet" {
   count = 3
 
   availability_zone       = data.aws_availability_zones.available.names[count.index]
-  cidr_block              = "10.0.${count.index}.0/24"
+  cidr_block              = "10.1.${count.index}.0/24"
   map_public_ip_on_launch = true
   vpc_id                  = aws_vpc.compute_vpc.id
   tags = map(
-    "Name", "terraform-eks-demo-node",
+    "Name", "compute-subnet",
     "kubernetes.io/cluster/${var.cluster-name}", "shared",
   )
 }
+
+
+resource "aws_subnet" "database_subnet" {
+  count = 3
+
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.2.${count.index}.0/24"
+  vpc_id                  = aws_vpc.database_vpc.id
+  tags = map(
+    "Name", "database-subnet")
+}
+
+resource "aws_subnet" "connectivity_subnet" {
+  count = 3
+
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  cidr_block              = "10.3.${count.index}.0/24"
+  vpc_id                  = aws_vpc.connectivity_vpc.id
+  tags = map(
+    "Name", "connectivity-subnet")
+}
+
 
 resource "aws_internet_gateway" "demo" {
   vpc_id = aws_vpc.compute_vpc.id
 
   tags = {
-    Name = "terraform-eks-demo"
+    Name = "internet-gateway"
   }
 }
 
@@ -46,8 +84,9 @@ resource "aws_route_table" "demo" {
 }
 
 resource "aws_route_table_association" "demo" {
-  count = 2
+  count = 3
 
   subnet_id      = aws_subnet.compute_subnet.*.id[count.index]
   route_table_id = aws_route_table.demo.id
 }
+
